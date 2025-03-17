6da674ebe25b4a3b3ef7841074b2349e9383248d
Daniil <danikcheban2007@gmail.com>
2025-03-18 02:43:08 +0300

    Update JwtFilter.java

--- src/main/java/danix/app/messenger_service/config/JwtFilter.java
+++ src/main/java/danix/app/messenger_service/config/JwtFilter.java
@@ -1,6 +1,5 @@
 package danix.app.messenger_service.config;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import danix.app.messenger_service.security.JWTUtil;
 import danix.app.messenger_service.security.UserDetailsServiceImpl;
 import danix.app.messenger_service.services.TokensService;
@@ -9,6 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +20,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
     private final UserDetailsServiceImpl detailsService;
@@ -28,21 +30,14 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
-            if (token.isBlank()) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-                return;
-            }
             try {
-                if (!tokensService.isValid(jwtUtil.getIdFromToken(token))) {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                    return;
+                String token = authHeader.substring(7);
+                if (token.isBlank()) {
+                    throw new AccessDeniedException("Invalid token");
                 }
-            } catch (IllegalStateException | JWTVerificationException e) {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-            try {
+                if (!tokensService.isValid(jwtUtil.getIdFromToken(token))) {
+                    throw new AccessDeniedException("Invalid token");
+                }
                 String email = jwtUtil.validateTokenAndRetrieveClaim(token);
                 UserDetails userDetails = detailsService.loadUserByUsername(email);
                 UsernamePasswordAuthenticationToken authenticationToken =
@@ -51,11 +46,16 @@ public class JwtFilter extends OncePerRequestFilter {
                 if (SecurityContextHolder.getContext().getAuthentication() == null) {
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                 }
-            } catch (JWTVerificationException e) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                filterChain.doFilter(request, response);
+            } catch (Exception e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             }
+        } else if (request.getRequestURI().startsWith("/auth") && !request.getRequestURI().equals("/auth/logout")) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
-        filterChain.doFilter(request, response);
+
     }
 }
 
--- src/main/java/danix/app/messenger_service/config/SecurityConfig.java
+++ src/main/java/danix/app/messenger_service/config/SecurityConfig.java
@@ -1,20 +1,16 @@
 package danix.app.messenger_service.config;
 
 import danix.app.messenger_service.security.UserDetailsServiceImpl;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
